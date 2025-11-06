d2c34d66dc1c17834c894d1e9313d3d3ef76bf31
DeathbatO13 <danielalejo99@gmail.com>
2025-11-06 18:55:03 -0500

    build

---
+++ build/CMakeFiles/wordle.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wordle.dir/src/Game.cpp.obj"
+  "CMakeFiles/wordle.dir/src/Game.cpp.obj.d"
+  "CMakeFiles/wordle.dir/src/InputHandler.cpp.obj"
+  "CMakeFiles/wordle.dir/src/InputHandler.cpp.obj.d"
+  "CMakeFiles/wordle.dir/src/Renderer.cpp.obj"
+  "CMakeFiles/wordle.dir/src/Renderer.cpp.obj.d"
+  "CMakeFiles/wordle.dir/src/Utils.cpp.obj"
+  "CMakeFiles/wordle.dir/src/Utils.cpp.obj.d"
+  "CMakeFiles/wordle.dir/src/WordList.cpp.obj"
+  "CMakeFiles/wordle.dir/src/WordList.cpp.obj.d"
+  "CMakeFiles/wordle.dir/src/main.cpp.obj"
+  "CMakeFiles/wordle.dir/src/main.cpp.obj.d"
+  "libwordle.dll.a"
+  "wordle.exe"
+  "wordle.exe.manifest"
+  "wordle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wordle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
